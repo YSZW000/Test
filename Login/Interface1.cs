56389153a4417bc4bcef88bbc43807746686993f
叶XX <2418181090@qq.com>
2019-03-21 10:27:10 +0800

    Signed-off-by: 叶XX <2418181090@qq.com>

---
+++ Login/Interface1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CardApp
+{
+    interface ILogin
+    {
+        //初始化{用户登陆信息保存在setting里面}
+        Login InitParam();
+        //是否显示界面
+        void ShowLoginView(bool IsShow);
+        //是否显示忘记密码
+        void ShowForgetPsw(bool IsShow);
+        //是否显示注册账号
+        void ShowRegister(bool IsShow);
+        //是否显示记住密码
+        void ShowRememberPsw(bool IsShow);
+        //是否显示自动登陆
+        void ShowAutoLogin(bool IsShow);
+        //登陆验证
+        User JudgePsw(Login login);
+    }
+}
---
+++ Login/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardApp
+{
+    public class Login
+    {   
+        public string LoginName { get; set; }
+        public string Passsword { get; set; }
+        public string ImageUrl { get; set; }
+        public bool RememberPsw { get; set; }
+        public bool AutoLogin { get; set; }
+    }
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string Age { get; set; }
+        public string Sex { get; set; }
+        public string Address { get; set; }
+        public string IDCard { get; set; }
+        public string Email { get; set; }
+    }
+}
